298111fa331b8a1e67a51893c9d0829fbf9ce8c8
Mohammednur <mamebb2023@gmail.com>
2024-09-17 13:02:02 +0300

    Convert the Temperature

---
+++ 2469. Convert the Temperature.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+      ans = []
+
+      kelvin = celsius + 273.15
+      fahrenheit = celsius * 1.80 + 32.00
+
+      ans = [kelvin, fahrenheit]
+      
+      return ans
+
+print(Solution.convertTemperature(Solution, -40)) # [-313.15, -40.0]
